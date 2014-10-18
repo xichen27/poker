8af40e5ebc6c9de77b82b3d8b5964a800be213dd
Xi Chen <xichen827@gmail.com>
2014-10-17 17:45:48 -0700

    Added hand beating calculations

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,12 +1,48 @@
 require_relative 'card'
 class Hand
-  
+  HANDS = Hash[:straight_flush?, 90,
+          :four_of_a_kind?, 80,
+          :full_house?, 70,
+          :flush?, 60,
+          :straight?, 50,
+          :three_of_a_kind?, 40,
+          :two_pairs?, 30,
+          :one_pair?, 20
+          ]
+          
   def initialize(cards)
     @cards = cards
   end
   
-  def beats?
-    
+  def beats?(other_hand)
+    if best_hand_value > other_hand.best_hand_value
+      return true
+    elsif best_hand_value == other_hand.best_hand_value
+      return true if beats_with_tie_breaker?(other_hand)
+    end
+    false
+  end
+  
+  def beats_with_tie_breaker?(another_hand)
+    case best_hand_value
+    when 90, 60, 50
+      puts " 2 SF / F / ST"
+      return true if card_values.max > another_hand.card_values.max
+    when 80, 70, 40, 20 #4 of a kind, full house, 
+      best_set = number_of_occurence.max_by {|k,v| v}
+      bsk = best_set.first
+  
+      best_set_other = another_hand.number_of_occurence.max_by {|k,v| v }
+      bsok = best_set_other.first
+      return true if bsk > bsok    
+    when 30
+      bpc =number_of_occurence.reject {|k, v| v == 1}.keys.max
+      bspok = another_hand.number_of_occurence.reject {|k, v| v == 1}.keys.max
+      return true if bpc > bspok
+    else 
+      puts "none of the above"
+    end
+    false
   end
   
   def straight_flush?
@@ -49,6 +85,12 @@ class Hand
     number_of_pairs == 2
   end
   
+  def best_hand_value
+    HANDS.each do |method, value|
+      return value if self.send(method)
+    end
+  end
+  
   def number_of_pairs
     number_of_pairs = 0
     number_of_occurence.each_value do |v|
@@ -72,23 +114,21 @@ class Hand
   
 end
 
-# [:straight?, :four_of_a_kind?, :full_house?].each do |method|
-#   self.send(method)
-# end
+
+
 
 #hand1 = Hand.new([Card.new(:deuce, :clubs), Card.new(:three, :clubs), 
  # Card.new(:four, :clubs), Card.new(:five, :clubs), Card.new(:six, :clubs)])
   
-hand2 = Hand.new([Card.new(:deuce, :clubs), Card.new(:deuce, :hearts), 
-    Card.new(:four, :clubs), Card.new(:four, :diamonds), Card.new(:four, :hearts)])  
-  
-# p hand1
-# p hand1.flush?
-# p hand1.straight?
-# p hand1.straight_flush?
-
-p hand2
-p hand2.full_house?
+# hand2 = Hand.new([Card.new(:deuce, :clubs), Card.new(:deuce, :hearts),
+#     Card.new(:four, :clubs), Card.new(:four, :diamonds), Card.new(:four, :hearts)])
+#
+straight_one = Hand.new([Card.new(:seven, :clubs), Card.new(:eight, :clubs),
+          Card.new(:nine, :clubs), Card.new(:ten, :clubs), Card.new(:jack, :clubs)]) 
+straight_two = Hand.new([Card.new(:seven, :clubs), Card.new(:eight, :clubs),
+                    Card.new(:nine, :clubs), Card.new(:ten, :clubs), Card.new(:jack, :clubs)]) 
+#p hand2
+p straight_one.beats_with_tie_breaker?(straight_two)
 # p hand1.straight?
 # p hand1.straight_flush?
   
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -2,45 +2,68 @@ require "hand"
 
 describe Hand do
   
-  let(:hand1) {Hand.new([Card.new(:deuce, :clubs), Card.new(:deuce, :hearts),
+  let(:hand_full_house) {Hand.new([Card.new(:deuce, :clubs), Card.new(:deuce, :hearts),
           Card.new(:four, :clubs), Card.new(:four, :diamonds), Card.new(:four, :hearts)])}
+    
+  let(:hand_full_house2) {Hand.new([Card.new(:deuce, :hearts), Card.new(:deuce, :spades),
+                  Card.new(:three, :hearts), Card.new(:three, :diamonds), Card.new(:three, :clubs)])}
         
-  let(:hand2) {Hand.new([Card.new(:ace, :club), Card.new(:six, :spades),
+  let(:hand_four_of_a_kind) {Hand.new([Card.new(:ace, :club), Card.new(:six, :spades),
           Card.new(:six, :hearts), Card.new(:six, :diamonds), Card.new(:six, :clubs)])}
           
-  let(:hand3) {Hand.new([Card.new(:seven, :clubs), Card.new(:eight, :clubs),
+  let(:hand_straight_flush) {Hand.new([Card.new(:seven, :clubs), Card.new(:eight, :clubs),
           Card.new(:nine, :clubs), Card.new(:ten, :clubs), Card.new(:jack, :clubs)])}
-          
-  let(:hand4) {Hand.new([Card.new(:deuce, :clubs), Card.new(:deuce, :hearts),
+ 
+  let(:hand_straight_flush2) {Hand.new([Card.new(:four, :hearts), Card.new(:five, :hearts),
+          Card.new(:six, :hearts), Card.new(:seven, :hearts), Card.new(:eight, :hearts)])}
+              
+  let(:hand_two_pair) {Hand.new([Card.new(:deuce, :clubs), Card.new(:deuce, :hearts),
           Card.new(:four, :clubs), Card.new(:four, :diamonds), Card.new(:seven, :hearts)])}
+  let(:hand_two_pair2) {Hand.new([Card.new(:deuce, :clubs), Card.new(:deuce, :hearts),
+                Card.new(:eight, :clubs), Card.new(:eight, :diamonds), Card.new(:seven, :hearts)])}
           
-
-
-
+          
+          
   it "should have 5 cards" do
     expect(Hand.new.cards.count).to eq(5)
   end  
   
   it "should recognize full house" do
-    expect(hand1).to be_full_house
+    expect(hand_full_house).to be_full_house
   end
 
   it "should recognize four of a kind" do
-    expect(hand2).to be_four_of_a_kind
+    expect(hand_four_of_a_kind).to be_four_of_a_kind
   end
     
   it "should recognize straight flush" do
-    expect(hand3).to be_straight_flush
+    expect(hand_straight_flush).to be_straight_flush
   end
   
   it "should recognize two pairs" do
-    expect(hand4).to be_two_pairs
+    expect(hand_two_pair).to be_two_pairs
   end 
 
-  # it "should have straight flush beat four of a kind" do
-#     expect(hand3.beats?(four_of_a_kind)).to be_true
-#   end
+  it "should have straight flush beat four of a kind" do
+    expect(hand_straight_flush.beats?(hand_four_of_a_kind)).to be_true
+  end
   
+  it "should not have four of a kind beat straight flush" do
+    expect(hand_four_of_a_kind.beats?(hand_straight_flush)).not_to be_true
+  end
+  
+  it "should beat another straight flush with lower numbers" do
+    expect(hand_straight_flush.beats?(hand_straight_flush2)).to be_true
+  end
+  
+  it "should beat another full house of lower triple set" do
+    expect(hand_full_house.beats?(hand_full_house2)).to be_true
+  end
+  
+  it "should beat another two pairs of lower highest pair" do
+    expect(hand_two_pair2.beats?(hand_two_pair)).to be_true
+  end
+
 
   
   
